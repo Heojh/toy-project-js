2dcc78ca38e74f64c76b58b1b81de911e56d9911
heojh <jihaeng1989@gmail.com>
2020-08-12 16:14:28 +0900

    word-chain > JavaScript: Mark a score

--- word-chain/main.js
+++ word-chain/main.js
@@ -4,7 +4,15 @@ const question = document.querySelector('.question');
 const answer = document.querySelector('.answer__text');
 const submitBtn = document.querySelector('.answer__btn');
 const wrong = document.querySelector('.wrong');
+const score = document.querySelector('.score');
 
+// Mark a score
+function paintScore(num) {
+    const getScore = Number(score.textContent.replace(/[^0-9]/g,'')) + num;
+    score.textContent = getScore >= 0 ? `점수 : ${getScore}` : `점수 : 0`;
+}
+
+// Compare Last and First Char
 function textCheck(event) {
     event.preventDefault();
     const answerText = answer.value;
@@ -16,9 +24,11 @@ function textCheck(event) {
         wrong.classList.add('invisible');
         question.textContent = answerText;
         answer.value = '';
+        paintScore(100);
     } else {
         wrong.classList.remove('invisible');
         answer.value = '';
+        paintScore(-50);
     }
 }
 
